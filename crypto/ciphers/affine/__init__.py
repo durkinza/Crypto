7e0549e984af07d5d11a3949aac0a9019ed3c7e7
Zane Durkin <zanedurkin@gmail.com>
2019-11-06 15:15:54 -0800

    setting message to optional, so we read from stdin by default

--- crypto/ciphers/affine/__init__.py
+++ crypto/ciphers/affine/__init__.py
@@ -10,13 +10,13 @@ def load(subparser):
     enc = sp.add_parser("encrypt", help="Encrypt the message")    
     enc.add_argument("--a", "-a", type=int, help="The first half of the key (a,b). a should be relatively prime to 26", required=True)
     enc.add_argument("--b", "-b", type=int, help="The second half of the key (a,b)", required=True)
-    enc.add_argument("--message", "-m", help="The Message", required=True)
+    enc.add_argument("--message", "-m", help="The Message")
 
         # Decrypt the message
     dec = sp.add_parser("decrypt", help="Decrypt the message")
     dec.add_argument("--a", "-a", type=int, help="The first half of the key (a,b). a should be relatively prime to 26", required=True)
     dec.add_argument("--b", "-b", type=int, help="The second half of the key (a,b)", required=True)
-    dec.add_argument("--message", "-m", help="The Message", required=True)
+    dec.add_argument("--message", "-m", help="The Message")
 
     obj = affine(parser)
     return obj
